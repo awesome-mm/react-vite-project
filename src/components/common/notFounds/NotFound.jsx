2ba3c42da157cf9822ddab0d2c51df00100cbac5
chan <opokonse123@naver.com>
2024-10-15 17:09:56 +0900

    not found page 이전페이지로 이동으로 수정

--- src/components/common/notFounds/NotFound.jsx
+++ src/components/common/notFounds/NotFound.jsx
@@ -1,4 +1,4 @@
-import {Navigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import {useEffect, useRef} from "react";
 import {NOT_FOUND_MSG} from "@/constants/errorText.js";
@@ -6,17 +6,19 @@ import {NOT_FOUND_MSG} from "@/constants/errorText.js";
 // 페이지를 요청하는 경우(404) 초기 화면으로 이동시킴
 const NotFound = () => {
   const toastId = useRef(null); // 중복 토스트 방지를 위한 ref
+  const navigate = useNavigate(); // useNavigate 훅 사용
 
-  const toHome = () => {
+  const toPreviousPage = () => {
     // 중복 토스트 방지
     if (!toast.isActive(toastId.current)) {
       toastId.current = toast.warn(NOT_FOUND_MSG);
     }
+    navigate(-1);
   };
 
-  useEffect(() => toHome(), []);
+  useEffect(() => toPreviousPage(), []);
 
-  return <Navigate replace to="/" />;
+  return null;
 };
 
 export default NotFound;
